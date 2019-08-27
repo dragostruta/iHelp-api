3011ce9bb708d0f2fdf01b0910638c9a057c5a5a
Truta Dragos <truta_dragos1997@icloud.com>
2019-08-27 03:30:27 +0300

    5: api token authentication

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -12,22 +12,6 @@ use Symfony\Component\HttpFoundation\Response;
 class SecurityController extends AbstractController
 {
     /**
-     * @Route("/login", name="app_login", methods={"POST"})
-     */
-    public function login(IriConverterInterface $iriConverter)
-    {
-        if (!$this->isGranted('IS_AUTHENTICATED_FULLY')){
-            return $this->json([
-                'error' => 'Invalid login request: check that the Content-Type is application/json',
-            ], 400);
-        }
-
-        return new Response(null, 204, [
-            'Location' => $iriConverter->getIriFromItem($this->getUser()),
-        ]);
-    }
-
-    /**
      * @Route("/logout", name="app_logout")
      */
     public function logout(){
